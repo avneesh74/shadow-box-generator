1c9142eec54d7f2b252c3b9fbcf226104e76a986
avneesh74 <avneeshjha19@gmail.com>
2020-05-06 17:23:34 +0530

    *

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Navigation from './Components/navbar'
+import FooterNavigation from './Components/footer'
+
 
 function App() {
+
+  const [Horizontal,setHorizontal] = useState(10)
+  const [Vertical,setVerticale] = useState(10)
+  const [Blur,setBlur] = useState(10)
+  const [Color,setColor]= useState('#006699')
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navigation 
+      logo={<img src= {require('./logo/logo.png')} alt="logo"/>}
+    />
+        <div className="App">
+        
+          <div className="control">
+            <label className="heading">Horizontal length</label>
+            <input type="range"  min="-200" max="200" value={Horizontal} onChange={(e)=>setHorizontal(e.target.value)}/>
+            <label  className="heading">Vertical length</label>
+            <input type="range"  min="-200" max="200" value={Vertical} onChange={(e)=>setVerticale(e.target.value)}/>
+            <label className="heading">Vertical length</label>
+            <input type="range"  min="0" max="200" value={Blur} onChange={(e)=>setBlur(e.target.value)}/>
+            <label className="heading">Select Color : </label>
+            <input type="color"  min="0" max="200" value={Color} onChange={(e)=>setColor(e.target.value)}/>
+          </div>
+          <div className="box-outer">
+            <div className="box-inner" style={{boxShadow:`${Horizontal}px ${Vertical}px ${Blur}px ${Color}`}} >
+                <p className="shadow-text"> box-shadow:{Horizontal}px {Vertical}px {Blur}px {Color} </p>
+            </div>
+          </div>
+          </div>
+          <FooterNavigation/>
     </div>
   );
 }
